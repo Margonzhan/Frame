8baa4e3377a62a2371ecb9f69fe069694cb94a0f
MaegonZhan <48540833+MaegonZhan@users.noreply.github.com>
2019-06-15 14:44:32 +0800

    io and motioin

---
+++ Fram/IOCard/IIoCard.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fram.IOCard
+{
+   public  interface IIoCard
+    {
+        /// <summary>
+        /// open and connect the io card
+        /// </summary>
+        /// <returns></returns>
+        bool Open();
+        /// <summary>
+        /// close and disconnect the iocard
+        /// </summary>
+        void  Close();
+        /// <summary>
+        /// get the input statue at index
+        /// </summary>
+        /// <param name="index">the  inout statue at index</param>
+        /// <returns></returns>
+        bool GetSingleInput(int index);
+        /// <summary>
+        /// get the output statue at index
+        /// </summary>
+        /// <param name="index">the output statue at index</param>
+        /// <returns></returns>
+        bool GetSingleOutput(int index);
+        /// <summary>
+        /// set the output statue at index 
+        /// </summary>
+        /// <param name="index">the io offset want to set</param>
+        /// <param name="value">the value want to set</param>
+        void  SetSingleOutput(int index,bool value);
+        /// <summary>
+        /// Batch acquisition of input status
+        /// </summary>
+        /// <param name="startindex"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        int GetMultiInput(int startindex,int offset);
+        /// <summary>
+        /// Batch acquisition of outout status
+        /// </summary>
+        /// <param name="startindex"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        int GetMultiOutput(int startindex, int offset);
+        /// <summary>
+        /// Batch setting of output status
+        /// </summary>
+        /// <param name="startindex"></param>
+        /// <param name="value"></param>
+        void SetMultiOutPut(int startindex,int value);
+    }
+}
---
+++ Fram/IOCard/IoCardBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fram.IOCard
+{
+   public  class IoCardBase:IIoCard
+    {
+        #region property
+        public string DeviceName { get; private set; }
+        public Guid Guid { get; set; }
+        public string CardType { get; private set; }
+        #endregion
+        #region method
+        public IoCardBase()
+        { }
+        public virtual bool Open() { return true; }
+        public virtual void Close() { }
+        public virtual bool GetSingleInput(int index) { return false; }
+        public virtual bool GetSingleOutput(int index) { return false; }
+        public virtual void SetSingleOutput(int index, bool value) { }
+
+        public virtual  int GetMultiInput(int startindex, int offset) { return 0; }
+        public virtual int GetMultiOutput(int startindex,int offset) { return 0; }
+        public virtual void SetMultiOutPut(int startindex,int value) { }
+        #endregion
+    }
+}
